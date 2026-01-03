ecb1bc1568d187a85468c6ec7cdce3270c811dec
CMN <charitylagsmedia@gmail.com>
2026-01-03 19:56:19 +0000

    Added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import '@vidstack/react/player/styles/default/layouts/video.css';
 import '@vidstack/react/player/styles/base.css';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+    <Analytics />
+  </StrictMode>
 )
