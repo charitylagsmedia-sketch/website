907251e052a2a64084a46c3c2adaa13d0337603d
CMN <charitylagsmedia@gmail.com>
2026-01-03 20:02:10 +0000

    added vercel speed-insights

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import App from './App.jsx'
 import '@vidstack/react/player/styles/default/layouts/video.css';
 import '@vidstack/react/player/styles/base.css';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </StrictMode>
 )
